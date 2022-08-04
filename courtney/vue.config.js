086bd78b5bb739854fc56c24d78b657da0bd4e83
Courtney <cjpike@bu.edu>
2022-08-04 09:58:43 -0400

    add resume vue

--- courtney/vue.config.js
+++ courtney/vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pages: {
+    'index': {
+      entry: 'src/pages/Home/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    '': {
+      entry: 'src/pages/Resume/main.js',
+      template: 'public/resume.html',
+      filename: 'resume.html',
+      title: 'Resume',
+      chunks: ['chunk-vendors', 'chunk-common', 'resume']
+    },
+  }
+}
