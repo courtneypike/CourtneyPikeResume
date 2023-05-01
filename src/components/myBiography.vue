de2809512eab65353c5a290d665a48cf919f66a7
Courtney <cjpike@bu.edu>
2023-05-01 11:21:33 -0400

    small update to biography

--- src/components/myBiography.vue
+++ src/components/myBiography.vue
@@ -16,7 +16,7 @@
             <img class = "biographyPic" src="/images_videos/biographypic2.jpeg" alt="Picture of Me">
         </div>
         <div class = "float-child biography">
-        <p>
+      <p>
         My name is Courtney Pike, and this is my site to demonstrate some of my professional experience and hobbies. <br><br>
 
         During my Master's degree at Boston University, I acquired advanced knowledge in computer networks, information systems analysis, network security, and IT strategy and management. I also gained hands-on experience in web application development, information structures with Python, and data visualization in R. I graduated with a cumulative GPA of 3.70/4.0. <br><br>
@@ -27,7 +27,7 @@
 
         Additionally, my internship as a Data Analyst at Bot Watt taught me valuable skills in data analysis and web scraping. I gained hands-on experience with Puppeteer, JavaScript, Node.js, and Sequelize ORM and learned to design, implement, and maintain data models that fit business needs. At this position, I wrote several web scraping programs to mine data from clean energy data sources routinely and integrated scraped data into the front-end by creating HTML pages that pull from the database using Web API routing, React, and Express.js.<br><br>
 
-        I have developed several projects that demonstrate my proficiency in various programming languages and frameworks, as well as my passion for full-stack development. An example of a project I have completed is coverify-stream.com (Coverify), a Spotify-approved application that pulls user streaming and profile data to make a user's Spotify history into a magazine cover. Coverify is a Spotify-approved application that uses express.js, serverless HTTP, and Netlify lambda through a middleware router to handle OAuth 2.0 framework with Spotify. As the sole developer, I built the front-end and design with JavaScript, CSS, and HTML. The website has had 1000+ visitors since its launch in April 2023.<br><br>
+        I have developed several projects that demonstrate my proficiency in various programming languages and frameworks, as well as my passion for full-stack development. An example of a project I have completed is <a href="coverify-stream.com">coverify-stream.com</a> (<a href="/coverify.html">Coverify</a>), a Spotify-approved application that pulls user streaming and profile data to make a user's Spotify history into a magazine cover. Coverify is a Spotify-approved application that uses express.js, serverless HTTP, and Netlify lambda through a middleware router to handle OAuth 2.0 framework with Spotify. As the sole developer, I built the front-end and design with JavaScript, CSS, and HTML. The website has had 1000+ visitors since its launch in April 2023.<br><br>
         </p>
         </div>
     </div>
