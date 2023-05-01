be523c7566f0cbfdcf3240f33d4abdc4f88b1a53
Courtney <cjpike@bu.edu>
2023-05-01 12:03:25 -0400

    optimize for mobile

--- src/components/myCoverify.vue
+++ src/components/myCoverify.vue
@@ -10,13 +10,13 @@
    <h1><a href="https://coverify-stream.com">Coverify</a></h1>
 
    <div class ="float-container">
-      <div class = "float-child coverify" style ="float:left; width: 40%">
+      <div class = "float-child coverifyPictures">
          <h2>Hover to See Examples of Coverify</h2>
          <section class="movingPictures">
           <img :src="selectedImg.src" @mouseleave="pictureHoverFunction()" @mouseover="pictureHoverFunction()" >
          </section>
       </div>
-   <div class = "float-child coverify" style ="float:left; width: 60%">
+   <div class = "float-child coverifyDescription">
    <ul>
       <li class = "coverify-bullet">Sole developer for Spotify-approved application that pulls relevant user streaming and profile data to make a user’s Spotify history into a magazine cover.</li>
       <li class = "coverify-bullet">1000+ visitors since launch of site in late April 2023.</li>
