d933988e3c82371e582aff448812059878bcd90a
Courtney <cjpike@bu.edu>
2023-05-02 09:38:10 -0400

    add youtube embeds; delete videos; fix typos

--- src/components/playingPiano.vue
+++ src/components/playingPiano.vue
@@ -14,29 +14,54 @@
         <p>I have been playing the piano for about 7 years now. I get weekly lessons, and have had a wide range of excellent teachers.</p>
     </div>
      <div style = "padding-top: 30px">
-        <h3>Pieces I have learned to play on the piano</h3>
+        <h3>Examples of some pieces I have learned to play on the piano</h3>
             <ul style = "width:50%; text-align: center; display: inline-block">
             <li class = "piano-list">Nocturne No.2 in E-Flat Major, Op. 9 No. 2 by Frederic Chopin</li> 
             <li class = "piano-list">Premerèie Gynmopedie by Eric Satie</li> 
             <li class = "piano-list">Clair de Lune, L. 32 by Claude Debussy</li> 
             <li class = "piano-list">Truman Sleeps by Philip Glass</li> 
             <li class = "piano-list">Goldberg Variations, BWV 988: Aria by Johann Sebastian Bach</li> 
+            <li class = "piano-list">Nocturne in C-Sharp Minor by Frederic Chopin</li> 
             </ul>
     </div>
 
       <h3 style = "padding-top: 30px">Videos of me playing piano</h3>
-        <h4>Nocturne in C-Sharp Minor by Frederic Chopin</h4> 
-        <video width="70%" height="70%" 
-        controls="controls"> 
-        <source src="/images_videos/piano.mp4" 
-            type="video/mp4"> 
-        </video>
+        <h4>Clair de Lune, L. 32 by Claude Debussy</h4> 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/n3TEUUR5v4o" 
+                title="YouTube video player" 
+                frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; 
+                gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe>
+
+        <h4  style = "border-top: 1px solid #16151b; padding-top: 30px>Three piece recital: <br>
+            Premerèie Gynmopedie by Eric Satie by Frederic Chopin <br>
+            Nocturne in C-sharp minor by Frederic Chopin<br>
+            Nocturne No.2 in E-Flat Major, Op. 9 No. 2 by Frederic Chopin <br>
+        </h4> 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/n3TEUUR5v4o" 
+                title="YouTube video player" 
+                frameborder="0" allow="accelerometer; 
+                autoplay; clipboard-write; encrypted-media; 
+                gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe></h4> 
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/j-HKCvhCLI0" 
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+                clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+        </iframe>
 
         <h4 style = "border-top: 1px solid #16151b; padding-top: 30px">Truman Sleeps by Philip Glass</h4> 
-        <video width="70%" height="70%" 
-        controls="controls"> 
-        <source src="/images_videos/truman_sleeps.mp4" 
-            type="video/mp4"> 
-        </video>
+        <iframe width="560" height="315" 
+                src="https://www.youtube.com/embed/X7M4D5-EOK0" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; 
+                autoplay; 
+                clipboard-write; 
+                encrypted-media; 
+                gyroscope; 
+                picture-in-picture; 
+                web-share" allowfullscreen>
+        </iframe>
     </body>
 </template>
