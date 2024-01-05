a0352d41ae7922d9d887d20352f476d45bdae1f0
Courtney <cjpike@bu.edu>
2024-01-05 10:30:13 -0600

    piano page center

--- src/components/playingPiano.vue
+++ src/components/playingPiano.vue
@@ -90,10 +90,12 @@ data() {
 <style>
 /* Add styles for the videos container */
 .videos {
+    width: 560px;
+    height: 315px;
+
     position: absolute;
+    top: 0; right: 0; 
+    bottom: 0; left: 0;
     margin: auto;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
 }
 </style>
